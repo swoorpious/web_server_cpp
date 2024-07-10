e7882da6ca80d9f6058ed7ec15435b0a3038b42c
Swaroop <swaroopshri2019@gmail.com>
2024-07-10 19:34:01 +0530

    Added a Common functions library

---
+++ Common/Common.h
@@ -0,0 +1,48 @@
+//
+// Created by Flowey on 10/07/2024.
+//
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <cstdlib>
+#include <cstring>
+#include <stdexcept>
+
+namespace Common
+{
+    template <typename T, typename U> 
+    T* AllocateBuffer(size_t BUFFER_SIZE, U INIT_BUFFER_VALUE)
+    {
+        try {
+            T *buffer = static_cast<T *>(std::malloc(sizeof(T) * BUFFER_SIZE));
+
+            if (buffer == nullptr) 
+                throw std::bad_alloc();
+
+            for (size_t i = 0; i < BUFFER_SIZE; i++) 
+                buffer[i] = static_cast<T>(INIT_BUFFER_VALUE);
+                
+            // std::memset(buffer, 0, sizeof(T) * BUFFER_SIZE);
+            return buffer;
+        } catch (std::bad_alloc& e) {
+            throw printf("Memory allocation failed: %s\n", e.what());
+        }
+        
+    };
+
+    template <typename T>
+    T* AllocateNullBuffer(size_t BUFFER_SIZE) {
+        
+    }
+    
+    void ClearBuffer() {
+        
+    };
+    void FreeBuffer() {
+        
+    };
+}
+
+
+#endif //COMMON_H
